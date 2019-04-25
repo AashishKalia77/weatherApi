ffd7fa74dd4ca407fa87c709fb15cc4e8428e854
Aashish Kalia <aashish.kalia@nagarro.com>
2019-04-25 19:30:13 +0530

    initial commit

--- src/app/dataservice.service.ts
+++ src/app/dataservice.service.ts
@@ -10,7 +10,7 @@ currentWeatherUrl='http://api.openweathermap.org/data/2.5/weather?units=metric&q
 url:string='http://api.openweathermap.org/data/2.5/forecast?units=metric&q=';
 authKey:string='&APPID=7be5a46933d5aa1e7c2571e63fbb5c4c'
   constructor(private http:HttpClient) { }
-
+  
   getFact(city:string){
     return this.http.get(this.url+city+this.authKey);
   }
